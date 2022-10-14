a20776f7556cdae2bd4a85f7e7d591fd9e41c14a
“Larissa” <souzalarissa383@gmail.com>
2022-10-14 12:58:10 -0300

    Revert "Delete src directory"
    This reverts commit 9dc34963e66fe271db18cdab7b75fdddf0975cfa.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Aplicacao from './Components/Resultado/Aplicacao'; 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Resultado</h1>
+
+        <Aplicacao/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
